a1e704f90bddf3cb22ab199a02ded8fdeb51a18c
Humberto Rojas <rojasadrianza@gmail.com>
2022-12-30 19:20:43 -0400

    Se agrego la primera parte de validaciones en el formulario de registro usando las validaciones de Angular

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router'
+import { Component, OnInit, Output, EventEmitter, NgModule } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators}  from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
+
+
+
+
+
+
 export class RegistroComponent implements OnInit {
 
-  
+
+
+registerForm = new FormGroup({
+  email: new FormControl('', [Validators.required, Validators.email]),
+  password: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(15),Validators.pattern("^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$")] ),
+  passwordConfirm: new FormControl('',Validators.required),
+},passwordsMustBeEqual);
+get emailControl(): FormControl{
+  return this.registerForm.get('email') as FormControl;
+}
+get passwordControl(): FormControl{
+  return this.registerForm.get('password') as FormControl;
+}
+
 
 constructor() {}
 
@@ -24,8 +43,10 @@ passwordIngresadoConfirmacion='';
 
   ngOnInit(): void {
 
-        
+   
   }
+
+  
 
   enviarUsuario(){    
     
@@ -41,7 +62,14 @@ passwordIngresadoConfirmacion='';
 
       this.parametros.emit(PARAMETROS);
 
-      //console.log(PARAMETROS);
+      this.registerForm.reset;
   }
 
 }
+
+export const passwordsMustBeEqual: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+  const password = group.get('password') as FormControl;
+  const passwordConfirm = group.get('passwordConfirm') as FormControl;
+
+  return password.value === passwordConfirm.value ? null : {passwordsMustBeEqual: true} 
+};
